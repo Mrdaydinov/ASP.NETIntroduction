87649dbb2dee50f556976059fd9f137382b43449
Murad <mrdaydinov@gmail.com>
2024-05-14 11:46:55 +0400

    init comm

---
+++ ASP.NETIntroduction/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETIntroduction.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            ViewData["Name"] = "SomeName1";
+            ViewData["Surname"] = "SomeSurname1";
+            ViewData["Age"] = "SomeAge1";
+            ViewData["Country"] = "SomeCountry1";
+            ViewData["PhoneNumber"]= "SomePhoneNumber1";
+
+            ViewBag.DataRowList = new List<string> { "SomeName2", "SomeSurname2", "SomeAge2", "SomeCountry2", "SomePhoneNumber2" };
+
+            return View();
+        }
+        
+    }
+}
